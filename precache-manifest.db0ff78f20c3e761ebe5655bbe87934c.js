510701b0334510e38f4c76fafb7d050125f66146
Admin <lethach150699@gmail.com>
2020-10-13 11:45:12 +0700

    Updates

---
+++ precache-manifest.db0ff78f20c3e761ebe5655bbe87934c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d92431995970dc68debd7e763287d88",
+    "url": "/masonry-reactjs/index.html"
+  },
+  {
+    "revision": "bd3a4c53cfd94fc5a9f8",
+    "url": "/masonry-reactjs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "045134c76f0844128bc1",
+    "url": "/masonry-reactjs/static/js/2.8bf64ad9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/masonry-reactjs/static/js/2.8bf64ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3a4c53cfd94fc5a9f8",
+    "url": "/masonry-reactjs/static/js/main.109f2426.chunk.js"
+  },
+  {
+    "revision": "45781268cfbe408b9bbb",
+    "url": "/masonry-reactjs/static/js/runtime-main.13deebd3.js"
+  }
+]);
